644c0c99b8b2e1dc43f2513d5eaec5408ec1b940
Singharaj Usai <san1999@att.net>
2022-06-25 19:29:10 -0400

    variables

--- Develop/script.js
+++ Develop/script.js
@@ -7,4 +7,23 @@ var interval = setInterval(function() {
                          .substring(0,3).toUpperCase());
     $('#currentTime').html(currentDate + " " + momentNow.format('hh:mm:ss a'))}, 100);
 
-  
+var hour = moment().hours();
+var userInput;
+var hourSpan;
+
+var sevenam = $(#07am);
+var eightam = $(#08am);
+var nineam = $(#09am);
+var tenam = $(#10am);
+var elevam = $(#11am);
+var twelvepm = $(#12pm);
+var onepm = $(#01pm);
+var twopm = $(#02pm);
+var threepm = $(#03pm);
+var fourpm = $(#04pm);
+var fivepm = $(#05pm);
+var sixpm = $(#06pm);
+var sevenpm = $(#07pm);
+var eightpm = $(#08pm);
+var ninepm = $(#09pm);
+var tenpm = $(#10pm);
